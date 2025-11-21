059d34b4e27bff3113b3fcecd2032f375d4a4587
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-11-21 06:40:08 +0100

    render map

--- client/map.js
+++ client/map.js
@@ -33,3 +33,28 @@ const playerSprites = [
 
 const playerImg = new Image();
 playerImg.src = "./assets/player1.png";
+
+// Render the map frame
+export function renderMap(map) {
+  // Clear previous game container if it exists
+  const existingGameContainer = document.getElementById("game-container");
+  if (existingGameContainer) existingGameContainer.remove();
+  const gameArea = document.querySelector("#game-area");
+  const mapTree = h("div", {
+    id: "game-container",
+    style: {
+      position: "relative", // Enables absolute positioning of children
+      gridTemplateColumns: `repeat(${MAP_SIZE}, ${TILE_SIZE}px)`,
+      gridTemplateRows: `repeat(${MAP_SIZE}, ${TILE_SIZE}px)`,
+      display: "grid",
+      gap: "0px",
+      border: "2px solid #333",
+      margin: "20px auto", // Center the map
+      height: `${MAP_SIZE * TILE_SIZE}px`,
+      width: `${MAP_SIZE * TILE_SIZE}px`,
+    },
+  });
+  const gameContainerElement = createDOMElement(mapTree);
+  gameArea.appendChild(gameContainerElement);
+  renderTiles(gameContainerElement, map);
+}
