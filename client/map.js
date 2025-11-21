2463e8bec1f6a6a039d8973ebb67464cd67cead8
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-11-21 06:38:49 +0100

    resources

--- client/map.js
+++ client/map.js
@@ -11,3 +11,25 @@ const COLORS = {
   [BREAKABLE_WALL]: "#bb6633",
   [EMPTY]: "#00ff80ff",
 };
+
+const STOPED_FRAME = 1; // the middle frame if the player is not moving
+
+const bombSprite = new Image();
+bombSprite.src = "./assets/bomb01.png";
+
+const flamesPowerupImg = new Image();
+flamesPowerupImg.src = "./assets/flame.png";
+
+const explosionSprite = new Image();
+explosionSprite.src = "./assets/explosion.gif";
+
+// Array of player spritesheets
+const playerSprites = [
+  "./assets/player1.png",
+  "./assets/player2.png",
+  "./assets/player3.png",
+  "./assets/player4.png",
+];
+
+const playerImg = new Image();
+playerImg.src = "./assets/player1.png";
