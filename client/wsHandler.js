8f4646095ec65fa339ece90041ad16b04175b782
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-12-01 17:20:41 +0100

    handle player

--- client/wsHandler.js
+++ client/wsHandler.js
@@ -11,3 +11,28 @@ import {
   renderExplosion,
   updateTile,
 } from "./map.js";
+
+export function handlePlayers(nickname, players) {
+  const waitingEl = document.querySelector(".waiting");
+  const playersListEl = document.querySelector(".players-list");
+
+  if (!waitingEl || !playersListEl) {
+    console.warn("handlePlayers: lobby UI not ready, skipping update");
+    return;
+  }
+  // Update basic game state
+  gameState.setState({
+    actualPlayerChat: nickname,
+    actualPlayer: nickname,
+    players: players,
+  });
+
+  if (players.includes(nickname)) {
+    waitingEl.innerHTML = "Need 2 players at least to start...";
+  }
+
+  playersListEl.innerHTML = `
+    Welcome <span> ${nickname}</span>,<br>
+    Current players:<br>- <span>${players.join("<br>- ")}</span><br>
+    (<span>${players.length}</span> / 4)`;
+}
